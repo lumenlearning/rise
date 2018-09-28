03214f5e46b75ec2f285121f5433e161e419dfa4
kalendar <david.wiley@gmail.com>
2018-09-28 11:01:14 -0600

    Updating tests to cover calculation of distance from origin

--- tests/testthat/test-rise.R
+++ tests/testthat/test-rise.R
@@ -6,8 +6,8 @@ library(ggplot2)
 p <- rise(sample_df)
 q <- rise(sample_df, visual = TRUE)
 
-test_that("Annotation happens...", {
-  expect_equal(ncol(p), 5 )
+test_that("Annotation happens correctly...", {
+  expect_equal(ncol(p), 6 )
 })
 
 test_that("RISE quadrant assignment happens correctly...", {
@@ -16,6 +16,10 @@ test_that("RISE quadrant assignment happens correctly...", {
 
 test_that("RISE inside/outside assignment happens correctly...", {
   expect_equal(is.numeric(p[,5]), TRUE )
+})
+
+test_that("Total distance calculation happens correctly...", {
+  expect_equal(is.numeric(p[,6]), TRUE )
 })
 
 test_that("RISE graphic is created...", {
